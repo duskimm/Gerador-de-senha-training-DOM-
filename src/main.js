37daeece87ad36846f8fc0923837b2f6031e2221
Duskim <duskimm@gmail.com>
2023-03-03 09:51:23 -0300

    05

--- src/main.js
+++ src/main.js
@@ -1,19 +1,16 @@
-import { nanoid } from 'nanoid';
+import { nanoid } from "nanoid";
 import copy from 'clipboard-copy';
 import './style.css';
 
-const GenBtn = document.querySelector('button');
+const GenPassqord = document.querySelector('button');
 const VisibleKey = document.querySelector('h2');
 
-GenBtn.addEventListener('click', () => {
+GenPassqord.addEventListener('click', () => {
   const randomPassword = nanoid();
   VisibleKey.innerHTML = randomPassword;
-  
 });
 
-const copyKey = copy;
-
-VisibleKey.addEventListener('click', function () {
-  copyKey(event.target.innerHTML);
-  alert('Senha copiada com sucesso')
+VisibleKey.addEventListener('click', (e) => {
+  copy(e.target.innerHTML);
+  alert('Senha copiada');
 });
